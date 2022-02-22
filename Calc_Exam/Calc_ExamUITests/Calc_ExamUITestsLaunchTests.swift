858b52f192e6494edbf7b41dd02b2e5d02627022
choijaegwon <chl9338@naver.com>
2022-02-23 00:11:34 +0900

    2022-02-23

---
+++ Calc_Exam/Calc_ExamUITests/Calc_ExamUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Calc_ExamUITestsLaunchTests.swift
+//  Calc_ExamUITests
+//
+//  Created by Jae kwon Choi on 2022/02/22.
+//
+
+import XCTest
+
+class Calc_ExamUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
